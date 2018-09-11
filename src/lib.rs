20bef3eb2c9e806182c189e0f4d5889b25da6690
Alex Beregszaszi <alex@rtfs.hu>
2018-09-11 23:58:37 +0100

    Add higher-level API

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,21 @@
 mod native;
+
+use std::ffi::CString;
+use std::ffi::CStr;
+
+pub fn version() -> String {
+    unsafe {
+        CStr::from_ptr(native::version()).to_string_lossy().into_owned()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::mem;
+
+    #[test]
+    fn test_version() {
+        assert_ne!(version().len(), 0);
+    }
+}
