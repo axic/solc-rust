eed7ea5d43fb8cc3232344f9e446731cb838d889
Alex Beregszaszi <alex@rtfs.hu>
2019-01-15 15:27:56 +0000

    Link against stdc++
    Code copied from https://github.com/pepyakin/binaryen-rs

--- build.rs
+++ build.rs
@@ -23,6 +23,11 @@ fn main() {
     println!("cargo:rustc-link-lib=static=devcore");
     println!("cargo:rustc-link-lib=static=jsoncpp");
 
+    // We need to link against C++ std lib
+    if let Some(cpp_stdlib) = get_cpp_stdlib() {
+        println!("cargo:rustc-link-lib={}", cpp_stdlib);
+    }
+
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
@@ -40,4 +45,21 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+// See https://github.com/alexcrichton/gcc-rs/blob/88ac58e25/src/lib.rs#L1197
+fn get_cpp_stdlib() -> Option<String> {
+    env::var("TARGET").ok().and_then(|target| {
+        if target.contains("msvc") {
+            None
+        } else if target.contains("darwin") {
+            Some("c++".to_string())
+        } else if target.contains("freebsd") {
+            Some("c++".to_string())
+        } else if target.contains("musl") {
+            Some("static=stdc++".to_string())
+        } else {
+            Some("stdc++".to_string())
+        }
+    })
 }
