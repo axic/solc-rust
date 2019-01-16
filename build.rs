6614077e46a1557003c6cb5cc290ae0c1e599028
Alex Beregszaszi <alex@rtfs.hu>
2019-01-16 01:50:09 +0000

    Disable z3/cvc4 in the Solidity build system

--- build.rs
+++ build.rs
@@ -10,6 +10,8 @@ fn main() {
     let dst = Config::new("solidity")
         .define("TESTS", "OFF")
         .define("TOOLS", "OFF")
+        .define("USE_Z3", "OFF")
+        .define("USE_CVC4", "OFF")
         .build();
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-search=native={}/build/libsolc", dst.display());
